26a7942a318cd9e04ed2703455005bbfc787c619
Harsha606 <136447394+Harsha606@users.noreply.github.com>
2023-09-26 15:01:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ session=Session.builder.configs(connection_parameters).create()
 col1,col2=st.columns(2)
 image = Image.open('LendingAI.png')
 st.image(image, width=250)
-selected_opt  = option_menu(None, ["Predictor App" ,"Defaulter App","Recommendation App","Segmentation",'Applications Data','Churn Data'],  
+selected_opt  = option_menu(None, ["Predictor App" ,"Defaulter App","Recommendation App","Segmentation",'Applications Data','Churn Data','Defaulter Data],  
 default_index=0, orientation="horizontal",icons=None,
                 menu_icon=None,
                 styles={              
@@ -142,8 +142,84 @@ if selected_opt =='Defaulter App':
                 st.plotly_chart(fig,use_container_width=True)
             else:
                 st.error("Entered Invalid data, Please check your Inputs...")
-if selected_opt == 'Recommendation App':
-    # Get data from Snowflake for the tables
-    transform_df = session.table("LENDINGAI_DB.BASE.TBL_ID_TABLE")
-    # Convert Snowflake DataFrames to pandas DataFrames
-    st.dataframe(transform_df.to_pandas())
+if selected_opt == 'Applications Data':
+    res = session.call('LENDINGAI_DB.MART.SP_APPLICATIONSCORE_LR_VALIDATIONPROC_SNOWPARK')
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        emp_length = st.selectbox("Experience:", ['< 1 year', '1 year', '2 years', '3 years', '4 years', '5 years', '6 years', '7 years', '8 years', '9 years', '10+ years'], key="emp_length")
+        debt_to_income_ratio = st.number_input('DTI RATIO:', min_value=0.0, key="debt_to_income_ratio")
+    with col2:
+        amount_requested = st.number_input('Loan Amount:', min_value=0, key="amount_requested")
+        loan_title = st.selectbox('Type of Loan:', ['Major purchase', 'Debt consolidation', 'Home improvement', 'Moving and relocation', 'Home buying', 'Business', 'Vacation', 'Car financing', 'Medical expenses', 'Credit card refinancing'], key="loan_title")
+    with col3:
+        risk_score = st.number_input('Credit Score:', min_value=0, key="risk_score")
+        application_status = st.selectbox("Select the application status", ['Approved', 'Rejected','Both'], key="application_status")
+    col4,col5=st.columns(2)
+    with col4:
+        age = st.selectbox('Age:', ['0-18', '18-24', '25-34', '35-44', '45-54', '55-64', '65+'])
+    with col5:
+        tenure = st.selectbox('Loan Repayment Tenure:', ['36 Months','60 Months'])
+    # Predict
+    if risk_score == 0:  # assuming default value of the input is 0 when nothing is entered
+        risk_score = 624
+    if debt_to_income_ratio >= 500:
+        risk_score += 10
+    elif debt_to_income_ratio >= 200:
+        risk_score += 20
+    elif debt_to_income_ratio >= 100:
+        risk_score += 30
+    elif debt_to_income_ratio >= 50:
+        risk_score += 50
+    elif debt_to_income_ratio >= 20:
+        risk_score += 60
+    elif debt_to_income_ratio >= 10:
+        risk_score += 70
+    else:
+        risk_score += 80
+    risk_score += debt_to_income_ratio
+    def is_valid_data(risk_score, amount_requested):
+        if (0 <= risk_score <= 1000 and amount_requested >= 0):
+            return True
+        return False
+    col11,col12,col13,col14,col15=st.columns(5)
+    with col13:
+        for _ in range(2):
+            st.write("")
+        btn4=st.button(':blue[Retrieve Related Applications]',key='button_cntr2',use_container_width=True)
+    if btn4:
+        if is_valid_data(risk_score, amount_requested):
+            lst = [risk_score, debt_to_income_ratio,emp_length,loan_title,amount_requested]
+            df = pd.DataFrame([lst], columns=['RISK_SCORE','DEBT_TO_INCOME_RATIO','EMPLOYMENT_LENGTH','LOAN_TITLE','AMOUNT_REQUESTED'])
+            snow_df = session.create_dataframe(df)
+            snow_df.write.mode("overwrite").saveAsTable("LENDINGAI_DB.BASE.TBL_APPLICATIONSCORE_APPLICATIONS_SNOWPARK")
+            res1 = session.sql('CALL LENDINGAI_DB.BASE.SP_APPLICATIONSCORE_APPLICANTIONS_SNOWPARK()').collect()
+            app_df = pd.DataFrame(res1)
+        if application_status == "Approved":
+            app_df = app_df[app_df['APPLICATION_STATUS'] == 1]
+        elif application_status == "Rejected":
+            app_df = app_df[app_df['APPLICATION_STATUS'] == 0]
+        elif application_status == "Both":
+             app_df = app_df[(app_df['APPLICATION_STATUS'] == 1) | (app_df['APPLICATION_STATUS'] == 0)]
+        final_appscore_df = app_df[['RISK_SCORE','DEBT_TO_INCOME_RATIO','EMPLOYMENT_LENGTH','LOAN_TITLE','AMOUNT_REQUESTED']]
+        fig2 = go.Figure(data=[go.Table(
+                columnwidth=[2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5],
+                header=dict(
+                    values=["<b>EMPLOYMENT LENGTH</b>", "<b>DEBT TO INCOME RATIO</b>", "<b>AMOUNT REQUESTED</b>", "<b>LOAN TITLE</b>", "<b>CREDIT SCORE</b>"],
+                    fill_color='#CDCDD6',
+                    font_color="#4C4C54",
+                    align=['center'],
+                    line_color='#ffffff',
+                    font_size=14,
+                    height=40
+                ),
+                cells=dict(values=[final_appscore_df.EMPLOYMENT_LENGTH,final_appscore_df.DEBT_TO_INCOME_RATIO,final_appscore_df.AMOUNT_REQUESTED,final_appscore_df.LOAN_TITLE,final_appscore_df.RISK_SCORE],fill_color = [['white','#f0f2f6']*3200], align=['center'], font_size = 12))])
+                # Update the layout of the Plotly table
+        fig2.update_layout(
+                    autosize=False,
+                    width=1400,
+                    height=400,
+                    margin=dict(l=0, r=0, b=0, t=0, pad=4),
+                    paper_bgcolor="#ffffff"
+                )
+        st.subheader("List of Applications")
+        st.plotly_chart(fig2)
