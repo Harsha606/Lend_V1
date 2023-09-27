f8f6c16b446d5e57a54a60a6ef3962f72d7720c3
Harsha606 <136447394+Harsha606@users.noreply.github.com>
2023-09-27 10:27:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -565,3 +565,4 @@ if selected_opt=='Defaulter Data':
                 )
                 st.subheader("List of Both Defaulted and Successful Applications")
                 st.plotly_chart(fig4)
+st.markdown("<div class='custom_footer'><b>Copyright (c) Anblicks Inc.  </b><a href='https://www.anblicks.com'>https://www.anblicks.com</a></div>", True)
